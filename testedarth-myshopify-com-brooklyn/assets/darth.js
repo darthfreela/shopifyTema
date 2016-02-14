55c062fbe7c77489512315b529d4a91bc5025a3c
darthfreela <darthfreela@gmail.com>
2016-02-14 14:12:11 -0200

    range 50%

--- testedarth-myshopify-com-brooklyn/assets/darth.js
+++ testedarth-myshopify-com-brooklyn/assets/darth.js
@@ -71,82 +71,16 @@ $(document).ready(function(){
 	//Elemento que vai receber os eventos.
     var nav = $('.site-header');
  	var hide = false;
-    //Ao rolar a página, a função será executada.
-    $(window).scroll(function () {
-        //Se o tamanho da parte rolada for maior que 136px (pixels) ->
-        if ($(this).scrollTop() > 750) {
-            nav.addClass("scrolled_menu"); //Aplica a classe scrolled_menu no elemento .menu
-            $('.bgmenu').off();
-            if(hide == false){
-            	$('.scrolled_menu').hide();
-            	hide = true;
-        	}
-            $('.scrolled_menu').slideDown(500);
-            //////////////////////////////////////////////////////////////////////////////////
-            //=========================MUDANÇA DA COR DOS LINKS E IMAGENS================// //
-            //////////////////////////////////////////////////////////////////////////////////
-            //link do cart
-            $('#logoa').css('display', 'none');
-			$('#logob').css('display', 'inline');
-            $('.linkscart ul>li>a').css('color', 'black');
-            $('#cart>a>span>svg>path').css('fill', 'black' );
-           	$('#AccessibleNav>li>a').css('color', 'black');
-           	$('#svgsearch>svg').css('fill', 'black' );
-           	$('#divSearch ul>li>a').css('color', 'black');
 
-        } else {
-        	$('#logoa').css('display', 'inline');
-			$('#logob').css('display', 'none');
-        	$('.linkscart ul>li>a').css('color', 'white');
-            $('#cart>a>span>svg>path').css('fill', 'white' );
-           	$('#AccessibleNav>li>a').css('color', 'white');
-           	$('#svgsearch>svg').css('fill', 'white' );
-           	$('#divSearch ul>li>a').css('color', 'white');
-            nav.removeClass("scrolled_menu"); //Remove a classe scrolled_menu no elemento .menu
-            //ESCUREÇER LINKS COM MOUSE MENU
-		$('.bgmenu').on('mouseover' ,function(){
-			$('#logoa').css('display', 'none');
-			$('#logob').css('display', 'inline');
-			$(this).css('background-color', 'white');
-			$('.linkscart ul>li>a').css('color', 'black');
-	        $('#cart>a>span>svg>path').css('fill', 'black' );
-	        $('#AccessibleNav>li>a').css('color', 'black');
-	        $('#svgsearch>svg').css('fill', 'black' );
-	        $('#divSearch ul>li>a').css('color', 'black');
-
-		});
-		$('.bgmenu').on('mouseleave', function(){
-			$('#logoa').css('display', 'inline');
-			$('#logob').css('display', 'none');
-			$(this).css('background-color', 'transparent');
-			$('.linkscart ul>li>a').css('color', 'white');
-	        $('#cart>a>span>svg>path').css('fill', 'white' );
-	        $('#AccessibleNav>li>a').css('color', 'white');
-	        $('#svgsearch>svg').css('fill', 'white' );
-	        $('#divSearch ul>li>a').css('color', 'white');
-			
-		});
-            hide = false;
-        }
-    });
 
 	//VERIFICAR QUAL PÁGINA É E ADICIONAR O HEADER
 	// PEGANDO O NOME DO DOCUMENTO OU PÁGINA ATUAL
 	documento = window.location.pathname;
  
 	if(documento=="/") {
+		alert('executei o baraaa');
 		$('.site-header').addClass('bgmenu');
-		//adicionar o popup aqui
-	}else{
-		$('.site-header').removeClass('bgmenu');
-		$('.linkscart ul>li>a').css('color', 'black');
-        $('#cart>a>span>svg>path').css('fill', 'black' );
-        $('#AccessibleNav>li>a').css('color', 'black');
-        $('#svgsearch>svg').css('fill', 'black' );
-        $('#divSearch ul>li>a').css('color', 'black');
-	}
-	
-	//ESCUREÇER LINKS COM MOUSE MENU
+		 //ESCUREÇER LINKS COM MOUSE MENU
 		$('.bgmenu').on('mouseover' ,function(){
 			$('#logoa').css('display', 'none');
 			$('#logob').css('display', 'inline');
@@ -169,6 +103,87 @@ $(document).ready(function(){
 	        $('#divSearch ul>li>a').css('color', 'white');
 			
 		});
-	
-	
+		//adicionar o popup aqui
+		//
+		 //Ao rolar a página, a função será executada.
+    	$(window).scroll(function () {
+	        //Se o tamanho da parte rolada for maior que 136px (pixels) ->
+	        if ($(this).scrollTop() > 750) {
+	            nav.addClass("scrolled_menu"); //Aplica a classe scrolled_menu no elemento .menu
+	           	$('.bgmenu').off();
+	            if(hide == false){
+	            	$('.scrolled_menu').hide();
+	            	hide = true;
+	        	}
+	            $('.scrolled_menu').slideDown(500);
+	            //////////////////////////////////////////////////////////////////////////////////
+	            //=========================MUDANÇA DA COR DOS LINKS E IMAGENS================// //
+	            //////////////////////////////////////////////////////////////////////////////////
+	            //link do cart
+	            $('#logoa').css('display', 'none');
+				$('#logob').css('display', 'inline');
+	            $('.linkscart ul>li>a').css('color', 'black');
+	            $('#cart>a>span>svg>path').css('fill', 'black' );
+	           	$('#AccessibleNav>li>a').css('color', 'black');
+	           	$('#svgsearch>svg').css('fill', 'black' );
+	           	$('#divSearch ul>li>a').css('color', 'black');
+
+	        } else {
+	        	$('#logoa').css('display', 'inline');
+				$('#logob').css('display', 'none');
+	        	$('.linkscart ul>li>a').css('color', 'white');
+	            $('#cart>a>span>svg>path').css('fill', 'white' );
+	           	$('#AccessibleNav>li>a').css('color', 'white');
+	           	$('#svgsearch>svg').css('fill', 'white' );
+	           	$('#divSearch ul>li>a').css('color', 'white');
+	            nav.removeClass("scrolled_menu"); //Remove a classe scrolled_menu no elemento .menu
+	           	hide = false;
+	           	$('.bgmenu').on('mouseover' ,function(){
+					$('#logoa').css('display', 'none');
+					$('#logob').css('display', 'inline');
+					$(this).css('background-color', 'white');
+					$('.linkscart ul>li>a').css('color', 'black');
+			        $('#cart>a>span>svg>path').css('fill', 'black' );
+			        $('#AccessibleNav>li>a').css('color', 'black');
+			        $('#svgsearch>svg').css('fill', 'black' );
+			        $('#divSearch ul>li>a').css('color', 'black');
+
+				});
+				$('.bgmenu').on('mouseleave', function(){
+					$('#logoa').css('display', 'inline');
+					$('#logob').css('display', 'none');
+					$(this).css('background-color', 'transparent');
+					$('.linkscart ul>li>a').css('color', 'white');
+			        $('#cart>a>span>svg>path').css('fill', 'white' );
+			        $('#AccessibleNav>li>a').css('color', 'white');
+			        $('#svgsearch>svg').css('fill', 'white' );
+			        $('#divSearch ul>li>a').css('color', 'white');
+					
+				});
+	        }
+    	});
+    	
+
+	}else{
+		
+		$('.site-header').removeClass('bgmenu');
+		$('#logob').css('display', 'inline');
+		$('#logoa').css('display', 'none');
+		$('.linkscart ul>li>a').css('color', 'black');
+        $('#cart>a>span>svg>path').css('fill', 'black' );
+        $('#AccessibleNav>li>a').css('color', 'black');
+        $('#svgsearch>svg').css('fill', 'black' );
+        $('#divSearch ul>li>a').css('color', 'black');
+       
+	}
+	//Muda a visualização dos produtos no range
+	$('#modeModel').on('click', function(){
+			alert('modeModel');
+	});
+	$('#modeProduct').on('click', function(){
+			alert('modeProduct');
+	});
+
+	//teste zoom
+	$("#imgz").addpowerzoom();
 });
